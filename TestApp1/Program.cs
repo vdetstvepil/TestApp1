ce8dbd4f9e4a4c6e39a8cee5aafffc869eba51dd
Anton Kislyakov <antonkis2012@gmail.com>
2023-01-23 11:32:31 +0300

    Add project files.

---
+++ TestApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+        }
+    }
+}
